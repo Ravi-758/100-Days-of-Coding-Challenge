cbea860e9b47e015a72b643016a801570528fb49
Ravi Kumar <56510822+Ravi-758@users.noreply.github.com>
2023-02-01 22:38:38 +0530

    Create Solution.java

---
+++ day 96 Abstract Class/Solution.java
@@ -0,0 +1,20 @@
+abstract class Sunstar {
+	abstract void printInfo();
+}
+class Employee extends Sunstar {
+	void printInfo() {
+		String name = "Ravi";
+		int age = 21;
+		float salary = 222.2F;
+
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(salary);
+	}
+}
+class Base {
+	public static void main(String args[]) {
+		Sunstar s = new Employee();
+		s.printInfo();
+	}
+}
